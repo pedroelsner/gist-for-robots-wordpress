9659a5b3d2fec5840dca43f0cb1d2b5b006a9fe7
Gabriel Koen <gabriel.koen@gmail.com>
2013-08-12 15:37:04 -0700

    Fixing PHP deprecated warning: eregi() is deprecated as of PHP 5.3.0

--- gist-for-robots-wordpress.php
+++ gist-for-robots-wordpress.php
@@ -21,12 +21,12 @@ Author URI: http://pedroelsner.com/
  * @return string
  */
 function shortcode_gist($atts, $content=null) {
-  
+
     extract(shortcode_atts(array(
       'id' => null,
       'file' => null,
-    ), $atts)); 
-   
+    ), $atts));
+
     $pattern = "/gist.github.com\/(\d+).js/";
     if ($content != null && $id == null & preg_match($pattern, $content, $matches)) {
 	  $id = $matches[1];
@@ -35,37 +35,37 @@ function shortcode_gist($atts, $content=null) {
     if ($content != null && $file == null & preg_match($pattern, $content, $matches)) {
 	  $file = $matches[1];
 	}
-    
+
     if ($id == null && $file == null) {
       return 'Gist id: '.$id.' file:'.$file;
     }
-    
-    
+
+
     $html = '<div class="gist-for-robots">';
-    
-    if (eregi('bot',$_SERVER['HTTP_USER_AGENT'])) {
-        
+
+    if ( strpos( $_SERVER['HTTP_USER_AGENT'], 'bot' ) !== false ) {
+
         $url = 'http://gist.github.com/' . trim($id) . '.json';
         $url .= $file != null ? '?file=' . trim($file) : '';
-        
+
         $json  = json_decode(file_get_contents($url), true);
         $html .= $json['div'];
         $html .= '<noscript>'.$json['div'].'</noscript>';
-    
+
     } else {
 
         if ($file == null) {
             $html .= '<script src="https://gist.github.com/' . $id . '.js"></script>';
         } else {
-        $html .= '<script src="https://gist.github.com/'.$id.'.js?file='.$file.'"></script>';
-    }
-    
+            $html .= '<script src="https://gist.github.com/'.$id.'.js?file='.$file.'"></script>';
+        }
+
     }
 
     $html .= '</div>';
-    
+
     return $html;
 }
 add_shortcode('gist', 'shortcode_gist');
- 
+
 ?>
